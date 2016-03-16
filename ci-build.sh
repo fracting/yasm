bfba2402d698ea218736e59038f2d20e6ea8ed60
Qian Hong <qhong@codeweavers.com>
2016-03-16 15:43:53 +0800

    Fix parameter.

--- ci-build.sh
+++ ci-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if test "platform" = "bash"; then
+if test "$platform" = "bash"; then
     echo apt-get install something
-elif test "platform" = "mingw32"; then
+elif test "$platform" = "mingw32"; then
     pacman --sync --noconfirm --noprogressbar mingw-w64-i686-gettext
 else
     echo "unknown environment!"
