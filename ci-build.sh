a7666a389f1c7a45e4d38b849016d4072a81c8f6
Qian Hong <qhong@codeweavers.com>
2016-03-16 16:02:03 +0800

    Fix msys32 shell.

--- ci-build.sh
+++ ci-build.sh
@@ -3,7 +3,7 @@
 if test "$platform" = "bash"; then
     echo "Building on Ubuntu Linux x86_64"
     sudo apt-get install build-essential automake
-elif test "$platform" = "mingw32"; then
+elif test "$platform" = "msys32"; then
     echo "Building on Msys2 i686"
 elif test "$platform" = "mingw32"; then
     echo "Building on MinGW-w64 i686"
